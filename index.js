bf287a04260cf082ea919bdde6b6900684f6bcdf
krishan <krishan14sharma@gmail.com>
2018-03-13 19:24:53 +0530

    heroku port number fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jsonParser = require('body-parser').json;
 const logger = require('morgan');
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(jsonParser());
 app.use('/ifttt', routes);
