f4a761a9b93f060ec050a983a6f43b7686d9fc49
krishan <krishan14sharma@gmail.com>
2018-03-15 17:30:16 +0530

    hiding

--- src/notification.js
+++ src/notification.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const serverKey="AAAA-PWVphs:APA91bHlTsW9nFTTAVXtDLZreoOzbFOT4ZACL8BvNVPBL1h2cYDJK8ajM615gvu_f9oJYGG7MJShpVxdfu8-ndFDSoe2kdzhsYQfE0Ln-TXhqJ4cCYYJfWgbSqadkN0MZDThWXmbCN56";
 const notificationPostUrl="https://gcm-http.googleapis.com/gcm/send";
 const hardCodedFCMDeviceKey="excGG_ogIrs:APA91bFjn2jxN8zPLwDBiSGkxGoTDhGpoQ_EPml0RmCzbiXd9VWo2FcjQOVzoRqdGwteC4DD1zhDs9gbWITCkmoEK9fUPQZ_zEZlPbV4L7jJ9kx5D3UmkyJZ8oyVcFZ3xgBm1ZFY8inl";
 
@@ -8,7 +7,7 @@ function sendNotification(res, message, id=null, speak=false) {
     let instance = axios.create({
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'key=' + serverKey
+            'Authorization': 'key=' + process.env.FIREBASE_SERVER_KEY
         }
     });
     instance.post(notificationPostUrl, {
